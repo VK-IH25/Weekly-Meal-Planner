6c8ac2b4bd75c84b5508885866c1132e5110cfa4
Victor Abussafi <victorabussafi@gmail.com>
2025-02-16 21:19:01 +0000

    install mantine

---
+++ Meal Planner/src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
+
+function App() {
+
+
+  return (
+
+    <MantineProvider>{  
+
+
+
+
+        <>
+      <div className="header">Hello World</div>
+      </>
+     
+     
+     
+     
+     
+     }</MantineProvider>
+
+  )
+}
+
+export default App
