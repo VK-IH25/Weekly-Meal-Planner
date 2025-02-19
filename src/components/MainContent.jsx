dc15f321430275337e1fe2389fd7b6879e6b8391
Kamran Ali <kamranali130893@gmail.com>
2025-02-19 19:05:52 +0100

    Mobile Resoonsive main table

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,5 +1,14 @@
 import React from "react";
-import { Flex, Table, Title, ScrollArea, Input, Button, CloseButton } from "@mantine/core";
+import {
+  Table,
+  Title,
+  ScrollArea,
+  Input,
+  CloseButton,
+  Container,
+  Grid,
+  Paper,
+} from "@mantine/core";
 import "../assets/styles/MainContent.css";
 import RecipeCards from "./RecipeCards";
 import { useState } from "react";
@@ -16,18 +25,25 @@ const MainContent = (props) => {
   ];
   const mealTimes = ["Breakfast", "Lunch", "Dinner"];
 
-
-
   const getRecipeName = (id) => {
-    return props.recipeList.find((r) => r.idMeal === id)?.strMeal || "Unknown Recipe";
+    return (
+      props.recipeList.find((r) => r.idMeal === id)?.strMeal || "Unknown Recipe"
+    );
   };
 
   // Function to remove a specific recipe from meal plan
   const clearRecipe = (day, mealTime, recipeId) => {
-    props.setMealPlan((prev) => prev.filter((meal) => !(meal.day === day && meal.mealTime === mealTime && meal.recipeId === recipeId)));
+    props.setMealPlan((prev) =>
+      prev.filter(
+        (meal) =>
+          !(
+            meal.day === day &&
+            meal.mealTime === mealTime &&
+            meal.recipeId === recipeId
+          )
+      )
+    );
   };
-
-
 
   //Drag and Drop
   const [dragging, setDragging] = useState(false);
@@ -42,7 +58,7 @@ const MainContent = (props) => {
   const handleDragEnter = (e) => {
     e.preventDefault();
     setDragging(true);
-  }
+  };
 
   const handleDrop = (ev, day, mealTime) => {
     ev.preventDefault();
@@ -52,11 +68,9 @@ const MainContent = (props) => {
       ...prev,
       { day, mealTime, recipeId }, // Add a new meal entry as an object
     ]);
-  
-  
 
     setDragging(false);
-  }
+  };
 
   //search bar
   const [query, setQuery] = useState("");
@@ -64,88 +78,107 @@ const MainContent = (props) => {
   const filteredList = props.recipeList.filter((recipe) =>
     Object.values(recipe).some(
       (value) =>
-        typeof value === "string" && value.toLowerCase().includes(query.toLowerCase())
+        typeof value === "string" &&
+        value.toLowerCase().includes(query.toLowerCase())
     )
   );
 
-
-
   return (
-    <Flex
-      mih={50}
-      gap="md"
-      justify="center"
-      align="center"
-      direction="row"
-    >
-
-      <div>
-        <ScrollArea h={500} id="origin"
-          onDrop={handleDrop}
-          onDragLeave={handleDragLeave}
-          onDragOver={handleDragOver}
-          onDragEnter={handleDragEnter}
-          bg={'var(--mantine-color-blue-light)'}
-
-          style={{ border: "1px solid gray", padding: "10px", borderRadius: "8px" }}>
-          <Input
-            placeholder="Search"
-            value={query}
-            mb="20px"
-            onChange={(event) => setQuery(event.target.value)}
-          />
-          <RecipeCards recipeList={filteredList}></RecipeCards>
-        </ScrollArea>
-
-      </div>
-      <div>
-        <Title order={3}>Weekly Meal Planner</Title>
-        <Table striped highlightOnHover withColumnBorders className={"meal-table"}>
-          <thead>
-            <tr>
-              <th>Meal Time</th>
-              {daysOfWeek.map((day) => (
-                <th key={day}>{day}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {mealTimes.map((mealTime) => (
-              <tr key={mealTime} style={{ height: "150px" }}>
-                <td>{mealTime}</td>
-                {daysOfWeek.map((day) => (
-                  <td
-                  key={day}
-                  id={`${day}${mealTime}`}
-                  onDrop={(ev) => handleDrop(ev, day, mealTime)}
-                  onDragLeave={handleDragLeave}
-                  onDragOver={handleDragOver}
-                  onDragEnter={handleDragEnter}
-                >
-                  {props.mealPlan
-                    .filter((meal) => meal.day === day && meal.mealTime === mealTime) // Filter meals for the current cell
-                    .map((meal) => (
-                      <div key={meal.recipeId} style={{ display: "flex", alignItems: "center", marginBottom: "5px" }}>
-                        {getRecipeName(meal.recipeId)}
-                        <CloseButton
-                          size="xs"
-                          style={{ marginLeft: "5px" }}
-                          onClick={() => clearRecipe(day, mealTime, meal.recipeId)}
-                        />
-                      </div>
+    <Container size="xl" px="md">
+      <Grid gutter="md" align="stretch">
+        <Grid.Col span={{ base: 12, sm: 3 }}>
+          <Paper shadow="xs" p="md" h="100%">
+            <ScrollArea
+              h={500}
+              id="origin"
+              onDrop={handleDrop}
+              onDragLeave={handleDragLeave}
+              onDragOver={handleDragOver}
+              onDragEnter={handleDragEnter}
+              bg={"var(--mantine-color-blue-light)"}
+              style={{
+                border: "1px solid gray",
+                padding: "10px",
+                borderRadius: "8px",
+              }}
+            >
+              <Input
+                placeholder="Search"
+                value={query}
+                mb="20px"
+                onChange={(event) => setQuery(event.target.value)}
+              />
+              <RecipeCards recipeList={filteredList}></RecipeCards>
+            </ScrollArea>
+          </Paper>
+        </Grid.Col>
+        <Grid.Col span={{ base: 12, sm: 9 }}>
+          <Paper shadow="xs" p="md" h="100%">
+            <Title order={3}>Weekly Meal Planner</Title>
+            <ScrollArea>
+              <Table
+                striped
+                highlightOnHover
+                withColumnBorders
+                className={"meal-table"}
+              >
+                <thead>
+                  <tr>
+                    <th>Meal Time</th>
+                    {daysOfWeek.map((day) => (
+                      <th key={day}>{day}</th>
                     ))}
-                </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </div>
-
-
-    </Flex>
+                  </tr>
+                </thead>
+                <tbody>
+                  {mealTimes.map((mealTime) => (
+                    <tr key={mealTime} style={{ height: "150px" }}>
+                      <td>{mealTime}</td>
+                      {daysOfWeek.map((day) => (
+                        <td
+                          key={day}
+                          id={`${day}${mealTime}`}
+                          onDrop={(ev) => handleDrop(ev, day, mealTime)}
+                          onDragLeave={handleDragLeave}
+                          onDragOver={handleDragOver}
+                          onDragEnter={handleDragEnter}
+                        >
+                          {props.mealPlan
+                            .filter(
+                              (meal) =>
+                                meal.day === day && meal.mealTime === mealTime
+                            ) // Filter meals for the current cell
+                            .map((meal) => (
+                              <div
+                                key={meal.recipeId}
+                                style={{
+                                  display: "flex",
+                                  alignItems: "center",
+                                  marginBottom: "5px",
+                                }}
+                              >
+                                {getRecipeName(meal.recipeId)}
+                                <CloseButton
+                                  size="xs"
+                                  style={{ marginLeft: "5px" }}
+                                  onClick={() =>
+                                    clearRecipe(day, mealTime, meal.recipeId)
+                                  }
+                                />
+                              </div>
+                            ))}
+                        </td>
+                      ))}
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </ScrollArea>
+          </Paper>
+        </Grid.Col>
+      </Grid>
+    </Container>
   );
 };
 
-
-export default MainContent
+export default MainContent;
