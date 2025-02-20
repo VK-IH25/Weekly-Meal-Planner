2e669312337cbcf454e32da1dc77b0d5c68b4009
Victor Abussafi <victorabussafi@gmail.com>
2025-02-20 08:11:57 +0000

    img fix

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,6 +1,10 @@
-import { Center, SimpleGrid } from '@mantine/core';
-import { Container, Image, Flex } from '@mantine/core';
-import { Link } from 'react-router';
+import { Center, SimpleGrid, Container, Image, Flex } from '@mantine/core';
+
+import { Link } from 'react-router-dom';
+import img10 from '../assets/img/10.png';
+import img8 from '../assets/img/8.png';
+import img9 from '../assets/img/9.png';
+import img11 from '../assets/img/11.png';
 
 function Board() {
 
@@ -15,17 +19,17 @@ function Board() {
         <Flex direction="column" align="center">
           {/* First row with three images */}
           <Flex gap={4} justify="center" align="center">
-            <Link to="recipe-list"><Image radius="md" src="src/assets/img/10.png" alt="Image 10" h={100} /></Link>
+            <Link to="recipe-list"><Image radius="md" src={img10} alt="Image 10" h={100} /></Link>
 
-            <Link to="about">        <Image radius="md" src="src/assets/img/8.png" alt="Image 8" w={200} />
+            <Link to="about">        <Image radius="md" src={img8} alt="Image 8" w={200} />
             </Link>
 
-            <Link to="mealplan">        <Image radius="md" src="src/assets/img/9.png" alt="Image 9" h={100} />
+            <Link to="mealplan">        <Image radius="md" src={img9} alt="Image 9" h={100} />
             </Link>
           </Flex>
 
           {/* Second row with image 11 */}
-          <Link to="planner">      <Image radius="md" src="src/assets/img/11.png" alt="Image 11" w={100} mt={4} />
+          <Link to="planner">      <Image radius="md" src={img11} alt="Image 11" w={100} mt={4} />
           </Link>
         </Flex>
       </Center>
